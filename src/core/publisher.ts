c80121b94c67009e58ce768f1ad5c788d206343d
Mohammed Muzammil Anwar <mohammad.muzammil100@gmail.com>
2026-05-09 01:30:14 +0530

    implement event publisher.

---
+++ src/core/publisher.ts
@@ -0,0 +1,29 @@
+import EventEmitter from 'node:events';
+import { loggerFor } from '../common';
+import type { DomainEvent } from './domain';
+import type { EventHandler } from '../types';
+
+const logger = loggerFor(import.meta.url);
+
+class EventPublisher {
+  public static emitter = new EventEmitter();
+
+  public static emit<EventType extends DomainEvent>(event: EventType) {
+    this.emitter.emit(event.name, event);
+  }
+
+  public static on<EventType extends DomainEvent>(event: string, listener: EventHandler<EventType>) {
+    this.emitter.on(event, this.wrapped(listener));
+  }
+
+  public static reset() {
+    this.emitter = new EventEmitter();
+  }
+
+  private static wrapped = <EventType extends DomainEvent>(handler: EventHandler<EventType>) => (event: EventType) => {
+    logger.info(`received event [${event.name}] for handler [${handler.name}] with payload:`, event?.payload);
+    return handler(event);
+  };
+}
+
+export { EventPublisher };
